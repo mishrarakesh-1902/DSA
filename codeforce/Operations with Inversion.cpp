5ac1b59b9b84596deff6065977e00670d76387c4
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2026-01-02 21:25:09 +0530

    optimal shift problem

---
+++ codeforce/Operations with Inversion.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin>>T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        vector<int> dp(n, 1);
+        for (int i=0;i<n;i++) {
+            for (intj=0;j<i;j++) {
+                if (a[j]<=a[i]) {
+                    dp[i]=max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int lnds=*max_element(dp.begin(), dp.end());
+        cout<<n-lnds<<'\n';
+    }
+    return 0;
+}
