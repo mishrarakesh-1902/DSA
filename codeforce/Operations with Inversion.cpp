7d85d3db0cb48f87778589af47674c14423bfef6
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2026-01-06 23:06:29 +0530

    new code add

--- codeforce/Operations with Inversion.cpp
+++ codeforce/Operations with Inversion.cpp
@@ -25,3 +25,38 @@ int main() {
     }
     return 0;
 }
+
+
+//new code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        int mn = a[n-1];
+        int operations = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            if (a[i] > mn) {
+                operations++;
+            }
+            mn = min(mn, a[i]);
+        }
+
+        cout<<operations<<'\n';
+    }
+
+    return 0;
+}
